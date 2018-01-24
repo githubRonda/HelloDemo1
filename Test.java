29810e68186f0ba24203170fb9afbc977c52d692
1575558177 <1575558177@qq.com>
2018-01-24 20:11:12 +0800

    add Test.java file

---
+++ Test.java
@@ -0,0 +1,17 @@
+
+import java.util.Arrays;
+
+class Test{
+	public static void man(String[] args){
+
+        String str = "我要去北京";
+        if (str.contains("要去")){
+
+            String[] splits = str.split("要去");
+
+            System.out.println(Arrays.toString(splits));
+
+            //Settings.System.putString(getContentResolver(), "inner_command", command + "_" + mRandom.nextInt(1000));//页面内部命令
+        }
+	}
+}
